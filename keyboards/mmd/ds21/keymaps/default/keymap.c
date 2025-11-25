5536085dc693a191d4d73c95ebfbb53cfa1fa5e3
AlpineWhite <phil_carty@msn.com>
2025-11-24 23:02:34 -0700

    correct minor config faults in era75 and mmd ds21

--- keyboards/mmd/ds21/keymaps/default/keymap.c
+++ keyboards/mmd/ds21/keymaps/default/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         LT(0, KC_MUTE), LT(1, KC_MPLY), LT(2, KC_MPLY), KC_BSPC,       LT(3, KC_MUTE),
 
-        MO(1),          KC_PSLS,        KC_PAST,        KC_PMNS,
+        LT(1, KC_BSPC), KC_PSLS,        KC_PAST,        KC_PMNS,
         KC_7,           KC_8,           KC_9,
         KC_4,           KC_5,           KC_6,           KC_PPLS,
         KC_1,           KC_2,           KC_3,           KC_PENT,
--- keyboards/womier/era75_wired/keymaps/default/config.h
+++ keyboards/womier/era75_wired/keymaps/default/config.h
@@ -1,7 +1,7 @@
 #define CAPS_LOCK_LED_INDEX                 44
 
-#define RGB_MATRIX_DEFAULT_ON true // Sets the default enabled state, if none has been set
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
-#define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
-#define RGB_MATRIX_DEFAULT_SAT 0 // Sets the default saturation value, if none has been set
-#define RGB_MATRIX_DEFAULT_VAL 40 // Sets the default brightness value, if none has been set
+//#define RGB_MATRIX_DEFAULT_ON true // Sets the default enabled state, if none has been set
+//#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+//#define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
+//#define RGB_MATRIX_DEFAULT_SAT 0 // Sets the default saturation value, if none has been set
+//#define RGB_MATRIX_DEFAULT_VAL 40 // Sets the default brightness value, if none has been set
