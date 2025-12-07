bba0a8fb12af03f897db60b10f769b80c1170302
AlpineWhite <phil_carty@msn.com>
2025-12-07 12:07:44 -0700

    push halconf comments

--- keyboards/womier/era75_wired/halconf.h
+++ keyboards/womier/era75_wired/halconf.h
@@ -4,8 +4,8 @@
 #pragma once
 
 #define HAL_USE_I2C TRUE
-#define HAL_USE_SERIAL TRUE
+//#define HAL_USE_SERIAL TRUE
 #define HAL_USE_SPI TRUE
-#define PAL_USE_CALLBACKS TRUE
+//#define PAL_USE_CALLBACKS TRUE
 
 #include_next <halconf.h>
